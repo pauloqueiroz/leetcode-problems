d380d92a1a5df6381fc890d6add40253ecf649d7
Paulo Sérgio <pauloqueiroz16@gmail.com>
2025-06-12 22:33:15 -0300

    Implementando problema numero palindromo

--- src/br/com/example/numbers/PalindromeNumber.java
+++ src/br/com/example/numbers/PalindromeNumber.java
@@ -5,23 +5,27 @@ public class PalindromeNumber {
 	public static void main(String[] args) {
 		System.out.println(9 % 10);
 		var algorithm = new PalindromeNumber();
-		System.out.println(algorithm.isPalindrome(-11));
+		//System.out.println(algorithm.isPalindrome(-11));
+		System.out.println(algorithm.isPalindrome(121));
+		System.out.println(algorithm.isPalindrome(122));
+		System.out.println(algorithm.isPalindrome(0));
+		System.out.println(algorithm.isPalindrome(10000));
 	}
 
 	public boolean isPalindrome(int x) {
-		if (x < -10) {
+		if(x<-9) {
 			return false;
 		}
-		if (x % 10 < 10) {
-			return true;
+		String xAsString = String.valueOf(x);
+		int j = xAsString.length()-1;
+		for(int i = 0; i < xAsString.length() || i<=j; i++) {
+			var charInicio = xAsString.charAt(i);
+			var charFim = xAsString.charAt(j);
+			if(charInicio != charFim) {
+				return false;
+			}
+			j--;
 		}
-		return false;
-		/*
-		 * int numInvertido; boolean aindaTemDigitos = true; int multiplicador = 1;
-		 * while(aindaTemDigitos) {
-		 * 
-		 * int num += num % (1*multiplicador) }
-		 */
-
+		return true;
 	}
 }
